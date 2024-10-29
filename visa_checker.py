2c11a29f74f39f2051f2c80e0bf744f889236fe2
jerickabuel-gwf <jerick.abuel@gwf.com.au>
2024-10-29 05:45:27 +0000

    update code

--- visa_checker.py
+++ visa_checker.py
@@ -6,24 +6,27 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-        return redirect(request.url)
-    if file:
-        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filepath)
-        data = pd.read_csv(filepath)
-        analysis = data.describe().to_html()
-        return render_template('analysis.html', tables=[analysis], titles=[''])
-    return redirect(url_for('index'))
+  if 'file' not in request.files:
+    return redirect(request.url)
+  file = request.files['file']
+  if file.filename == '':
+    return redirect(request.url)
+  if file:
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(filepath)
+    data = pd.read_csv(filepath)
+    analysis = data.describe().to_html()
+    return render_template('analysis.html', tables=[analysis], titles=[''])
+  return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
